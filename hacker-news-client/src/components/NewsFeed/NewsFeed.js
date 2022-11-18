a9c9eb149da44277a10cede909068ef2501194ab
Eleonora Nikerina <eleonora.radina@mail.ru>
2022-11-19 01:12:54 +0400

    add gh pages support

--- hacker-news-client/src/components/NewsFeed/NewsFeed.js
+++ hacker-news-client/src/components/NewsFeed/NewsFeed.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { changeNewsFeed } from '../../store/NewsFeed/actions'
 import NewsCard from '../NewsCard/NewsCard';
